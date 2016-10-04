7041409e083545499bf53f19602d1eec1173bc98
Alejandro Villegas alonso <avillegasal@axpecantabria.com>
2016-10-04 17:56:21 +0200

    avances

--- brutalSoccer/Program.cs
+++ brutalSoccer/Program.cs
@@ -11,9 +11,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using Accord.MachineLearning.Bayes;
+using System.Data;
+using Accord.Statistics.Filters;
+using Accord;
+using Accord.Math.Optimization.Losses;
+
 namespace brutalSoccer
 {
-   
+
     class Program
     {
         [DllImport("kernel32.dll", SetLastError = true)]
@@ -23,31 +29,88 @@ namespace brutalSoccer
         static void Main(string[] args)
         {
             AllocConsole();
-            EjecucionPostCreacion();
-            // Create a simple binary AND
-            // classification problem:
-
-            double[][] problem =
+            // Let's say we have the following data to be classified
+            // into three possible classes. Those are the samples:
+            // 
+            double[][] inputs =
             {
-    //             a    b    a + b
-    new double[] { 0,   0,     0    },
-    new double[] { 0,   1,     0    },
-    new double[] { 1,   0,     0    },
-    new double[] { 1,   1,     1    },
+    //               input         output
+    new double[] { 0, 1, 1, 0 }, //  0 
+    new double[] { 0, 1, 0, 0 }, //  0
+    new double[] { 0, 0, 1, 0 }, //  0
+    new double[] { 0, 1, 1, 0 }, //  0
+    new double[] { 0, 1, 0, 0 }, //  0
+   
+   
+   
+
 };
 
-            // Get the two first columns as the problem
-            // inputs and the last column as the output
+            int[] outputs = // those are the class labels
+            {
+    0, 0, 0, 0, 1,
+  
 
-            // input columns
-            double[][] inputs = problem.GetColumns(0, 1);
+};
 
-            // output column
-            int[] outputs = problem.GetColumn(2).ToInt32();
+            // Create a one-vs-one multi-class SVM learning algorithm 
+            var teacher = new MulticlassSupportVectorLearning<Gaussian>()
+            {
+                // Configure the learning algorithm to use SMO to train the
+                //  underlying SVMs in each of the binary class subproblems.
+                Learner = (param) => new SequentialMinimalOptimization<Gaussian>()
+                {
+                    // Estimate a suitable guess for the Gaussian kernel's parameters.
+                    // This estimate can serve as a starting point for a grid search.
+                    UseKernelEstimation = true,
+                    UseComplexityHeuristic = true
+                 
+                }
+            };
 
-            // Plot the problem on screen
-            ScatterplotBox.Show("AND", inputs, outputs).Hold();
+            // Configure parallel execution options
+            teacher.ParallelOptions.MaxDegreeOfParallelism = 1;
 
+            // Learn a machine
+            var machine = teacher.Learn(inputs, outputs);
+
+
+
+            // Create the multi-class learning algorithm for the machine
+            var calibration = new MulticlassSupportVectorLearning<Gaussian>()
+            {
+                Model = machine, // We will start with an existing machine
+
+                // Configure the learning algorithm to use SMO to train the
+                //  underlying SVMs in each of the binary class subproblems.
+                Learner = (param) => new ProbabilisticOutputCalibration<Gaussian>()
+                {
+                    Model = param.Model // Start with an existing machine
+                }
+            };
+
+
+            // Configure parallel execution options
+            calibration.ParallelOptions.MaxDegreeOfParallelism = 1;
+
+            // Learn a machine
+            calibration.Learn(inputs, outputs);
+
+            // Obtain class predictions for each sample
+            int[] predicted = machine.Decide(inputs);
+
+            // Get class scores for each sample
+            double[] scores = machine.Score(inputs);
+
+            // Get log-likelihoods (should be same as scores)
+            double[][] logl = machine.LogLikelihoods(inputs);
+
+            // Get probability for each sample
+            double[][] prob = machine.Probabilities(inputs);
+
+            // Compute classification error
+            double error = new ZeroOneLoss(outputs).Loss(predicted);
+            double loss = new CategoryCrossEntropyLoss(outputs).Loss(prob);
         }
 
         private static void EjecucionPostCreacion()
